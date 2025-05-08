652b441a101564ff22474db7de6f7f81947284bc
rems <rems@gmail.com>
2025-05-08 14:34:47 -0400

    feat: added library for Test

---
+++ src/Calculator.java
@@ -0,0 +1,23 @@
+import java.util.stream.DoubleStream;
+
+/**
+ * Classe pour éxécuter des actions mathématiques
+ * Utils pour la preuve de concept des tests
+ */
+public class Calculator {
+
+    double add(double... operands) {
+        return DoubleStream.of(operands)
+                .sum();
+    }
+
+    double multiply(double... operands) {
+        return DoubleStream.of(operands)
+                .reduce(1, (a, b) -> a * b);
+    }
+
+    double divide(double a, double b) {
+        if (b == 0) throw new ArithmeticException("Division by zero");
+        return a / b;
+    }
+}
