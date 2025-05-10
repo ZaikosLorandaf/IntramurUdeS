edc30a1c1fb801e01f4409a270649fff5385c83e
rems <rems@gmail.com>
2025-05-10 13:31:59 -0400

    feat: Ajout de héritage en DAO

---
+++ src/base/DAO/AppDAO.java
@@ -0,0 +1,45 @@
+package base.DAO;
+
+import base.LoggerUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AppDAO extends BaseDAO {
+
+    /**
+     * Permet de récupérer le rôle d'un utilisateur
+     * @param username Nom d'utilisateur
+     * @param password Mot de passe
+     * @return Le nom du rôle ou null si non trouvé
+     */
+    public static String getUserRole(String username, String password) {
+        String role = null;
+        String query = """
+            SELECT r.roleName 
+            FROM user u 
+            JOIN role r ON u.role_id = r.id 
+            WHERE u.username = ? AND u.password = ?
+        """;
+
+        try (Connection c = getConnection();
+             PreparedStatement stmt = c.prepareStatement(query)) {
+
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    role = rs.getString("roleName");
+                }
+            }
+        } catch (SQLException e) {
+            LoggerUtil.error("Erreur lors de la récupération du rôle de l'utilisateur.");
+        }
+
+        return role;
+    }
+}
+
---
+++ src/base/DAO/BaseDAO.java
@@ -0,0 +1,23 @@
+package base.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BaseDAO {
+    private static Connection connection;
+
+    protected static Connection getConnection() {
+        if (connection == null) {
+            try {
+                Class.forName("org.sqlite.JDBC");
+                connection = DriverManager.getConnection("jdbc:sqlite:IntraMurUdeS.db");
+                System.out.println("Opened database successfully");
+            } catch (Exception e) {
+                System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                System.exit(0);
+            }
+        }
+        return connection;
+    }
+}
