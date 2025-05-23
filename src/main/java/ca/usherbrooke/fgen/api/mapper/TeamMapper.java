ee3b01bc837f4fb7055e659a061baa5bd5560014
2030706-claurendeau <misterracoon@hotmail.com>
2025-05-23 15:52:08 -0400

    Mapper Team et formatage textuel de "init.sql"

---
+++ src/main/java/ca/usherbrooke/fgen/api/mapper/TeamMapper.java
@@ -0,0 +1,15 @@
+package ca.usherbrooke.fgen.api.mapper;
+
+import ca.usherbrooke.fgen.api.backend.Team;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface TeamMapper {
+    List<Team> selectTeams();
+    Team selectOneTeam(@Param("id") int id);
+    void deleteOneTeam(@Param("id") int id);
+    void insertTeam(Team team);
+}
