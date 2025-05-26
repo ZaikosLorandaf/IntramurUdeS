467865a3c24b999805b76ae9e4aa076f95cc3967
AtomicFrogg <pico2758@usherbrooke.ca>
2025-05-26 11:04:10 -0400

    L'authentification arrete de cracher (comme une chevre)

--- docker/keycloak/startup-scripts/init.sh
+++ docker/keycloak/startup-scripts/init.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-timeout 300 /bin/bash -c 'until curl -sI -o /dev/null -w "%{http_code}\n" localhost:8180; do sleep 5; done;'
+# Appel du processus d'attente du démarrage de keycloak et ajout des usage
+echo "beginning of installation..."
 
-echo "beginning of finalisation ...."
-/opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8180/ --realm master --user admin --password admin
-/opt/keycloak/bin/kcadm.sh create realms -s realm=usager -s enabled=true -o
-/opt/keycloak/bin/kcadm.sh create clients -r usager -f /var/tmp/frontend.json
-/opt/keycloak/bin/kcadm.sh create clients -r usager -f /var/tmp/backend.json
-/opt/keycloak/bin/kcadm.sh create partialImport -r usager -s ifResourceExists=OVERWRITE -f /var/tmp/users.json
+/var/tmp/setdata.sh &
+/opt/keycloak/bin/kc.sh start-dev --http-port 8180
 
-echo -e -n "\r"
-echo "server running ...."
+
