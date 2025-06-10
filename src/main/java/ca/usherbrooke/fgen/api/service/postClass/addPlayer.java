c6a832f57af6a462aead87820ee95fe6cd75277f
AtomicFrogg <pico2758@usherbrooke.ca>
2025-06-10 12:28:30 -0400

    asfdaf

--- src/main/java/ca/usherbrooke/fgen/api/service/postClass/addPlayer.java
+++ src/main/java/ca/usherbrooke/fgen/api/service/postClass/addPlayer.java
@@ -2,11 +2,17 @@ package ca.usherbrooke.fgen.api.service.postClass;
 
 import io.smallrye.common.constraint.NotNull;
 
-public class addTeam {
+public class addPlayer {
     @NotNull
     public String nomLigue;
     @NotNull
     public String nomSport;
     @NotNull
     public String nomTeam;
+    @NotNull
+    public int number;
+    @NotNull
+    public String nom;
+    @NotNull
+    public String prenom;
 }
