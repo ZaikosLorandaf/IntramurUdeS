1ceea4bc1747f66ef80d06ebad9f453b705b2fdb
rems <rems@gmail.com>
2025-06-30 10:12:35 -0400

    feat: test should pass

---
+++ src/test/java/ca/usherbrooke/fgen/api/service/GetDashboardTest.java
@@ -0,0 +1,19 @@
+package ca.usherbrooke.fgen.api.service;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
+class GetDashboardTest {
+//    @Test
+//    void testNothingEndpoint() {
+//        RestAssured.given()
+//                .when().get("api/dashboard/nothing")
+//                .then()
+//                .statusCode(200)
+//                .body(is("---"));
+//    }
+} 
