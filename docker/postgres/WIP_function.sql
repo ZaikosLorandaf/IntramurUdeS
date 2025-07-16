6d7f133ee15f5719109c8a9fb42adc521f2b4a79
Jacob <beaj1451@usherbrooke.ca>
2025-07-16 18:56:14 -0400

    Gestion de la liste des équipes dans l'ajout de match
    Took 20 minutes

--- docker/postgres/WIP_function.sql
+++ docker/postgres/WIP_function.sql
@@ -7,6 +7,10 @@ AS $$
     DECLARE
         id_new_match INT;
 BEGIN
+    IF(new.place IS NULL)
+    THEN
+        new.place = '--';
+    END IF;
     INSERT INTO intramurudes.match_ (date_match, begin_time, end_time, id_league, id_season, place)
     VALUES(new.date_match, new.begin_time, new.end_time, new.id_league, new.id_season, new.place);
     id_new_match = (SELECT last_value FROM intramurudes.match__id_seq);
